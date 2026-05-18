f27c9e9519c4c6274a73551e5ca9e906b0f3bc28
Natanael Borges <natanaelborges2004@gmail.com>
2026-05-18 11:16:36 -0300

    feat: adiciona exercício de paridade

---
+++ fucapi/periodo2/paradigmas_lp/impar_par.py
@@ -0,0 +1,32 @@
+"""
+REPOSITÓRIO: formacao-software-eng
+LOCALIZAÇÃO: fucapi/periodo2/paradigmas_lp/par_ou_impar.py
+CONCEITOS: Operadores Aritméticos (Módulo), Estruturas Condicionais (Match Case)
+
+ENUNCIADO:
+    Faça um programa que:
+    Leia um número inteiro; 
+    Informe se ele é par ou ímpar.
+"""
+
+def verificar_paridade():
+    try: # Bloco Try Except para capturar erro
+        # Entrada de dados
+        num = int(input("Digite um número inteiro: "))
+
+        """
+        Calculo para saber se o número é impar
+        if num % 2 == 0 (Resto da divisão por 2 == 0), o número é par
+        else (Resto da divisão qualquer valor além de 0), o número é ímpar
+        """
+        resultado = 'Par' if num % 2 == 0 else 'Ímpar'
+
+        # Saída de dados
+        print(f'Resultado: O número {num} é {resultado}.')
+
+    except ValueError:
+        # Mensagem de erro
+        print("Erro: Insira valores numéricos.")
+
+if __name__ == '__main__':
+    verificar_paridade()
